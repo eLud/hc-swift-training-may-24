3d4697811acb58b69fe02800fb14b393d0b62766
Ludovic Ollagnier <ludovic@tectec.fr>
2024-05-14 10:09:32 +0200

    Update to example project

---
+++ Projects/Move/Move/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Move
+//
+//  Created by Ludovic Ollagnier on 14/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
